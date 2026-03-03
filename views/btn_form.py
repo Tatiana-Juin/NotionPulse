19378f99e15a67962a5c236827ba7c52720cc59d
Tatiana-Juin <username@users.noreply.github.com>
2026-03-03 13:21:20 +0100

    J'ai créer le bouton et je l'ai importer dans form et tout fonctionne

--- views/btn_form.py
+++ views/btn_form.py
@@ -1,8 +1,7 @@
 import customtkinter
 
 class BtnForm(customtkinter.CTkButton):
-    def __init__(self,master):
-        super().__init__(master,fg_color="#F9F9F9")
+    def __init__(self,master,**kwargs):
+        super().__init__(master, text="Valider", **kwargs)
 
-        self.btn = customtkinter.CTkButton(self,text="Valider")
-        self.btn.grid(row=2,column=2,pady=3,padx=3)
+       
--- views/form.py
+++ views/form.py
@@ -1,5 +1,5 @@
 import customtkinter
-# from btn_form import BtnForm
+from  .btn_form import BtnForm
 
 class Form(customtkinter.CTkFrame):
     def __init__(self,master):
@@ -8,6 +8,6 @@ class Form(customtkinter.CTkFrame):
         self.question = customtkinter.CTkEntry(self,placeholder_text="Test a saisir");
         self.question.grid(row=2,column=1, padx=3,pady=3)
 
-        # self.btn_frame = BtnForm(self)
-        # self.btn_frame.grid(row=2,column=2,pady=3,padx=3)
+        self.btn_frame = BtnForm(self)
+        self.btn_frame.grid(row=2,column=2,pady=3,padx=3)
 

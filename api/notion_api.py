525851579fbecec1298f3687087de25aa2a22030
Tatiana-Juin <username@users.noreply.github.com>
2026-03-03 18:36:15 +0100

    Je peux ajouter des question dans mon notion sa fonctionne

---
+++ api/notion_api.py
@@ -0,0 +1,73 @@
+import customtkinter
+import requests
+import json
+from .config_api import Config
+
+class NotionAPI:
+    BASE_URL = "https://api.notion.com/v1"
+    
+    headers = {
+        "Authorization": f"Bearer {Config.TOKEN}",
+        "Content-Type": "application/json",
+        "Notion-Version": "2022-06-28"
+    }
+
+    # VERIFICATION 
+    @classmethod
+    def verification(cls,question):
+        question = question.strip()
+        # Pour etre sur que l'on a inserer une question 
+        if not question: 
+            return False, "erreur tu dois poser une question"
+        
+        # rechercher si une question existe deja dans la DB 
+        url = f"{cls.BASE_URL}/databases/{Config.DB_QUESTIONS}/query"
+
+        # On filtre les question de la db
+        payload = {
+            "filter": {
+                "property":"Nom",
+                "title": {
+                    "equals": question
+                }
+            }
+        }
+        # envoyer la requete 
+        response = requests.post(url,headers=cls.headers,json=payload)
+        # si la reponse retourne 200 aucune erreur avec api 
+        if response.status_code==200:
+            donnees = response.json()
+            # verification de la taille du tableau 
+            if len(donnees["results"]) > 0:
+                return False,"Cette question existe deja"
+            else:
+                return True,"la question est nouvelle"
+        else:
+            return False, f"Erreur api {response.status_code}"
+    
+    # AJOUT DE LA QUESTION 
+    @classmethod
+    def ajout_question(cls,question):
+        # POUR CREER UNE NOUVELLE LIGNE, POUR L'INSERTION 
+        url = f"{cls.BASE_URL}/pages"
+
+        payload = {
+            "parent" : {"database_id": Config.DB_QUESTIONS},
+            "properties":{
+                "Nom":{
+                    "title":[{"text": {"content": question}}]
+                },
+                "Description":{
+                    "rich_text":[{"text": {"content":"depuis mon application"}}]
+                }
+            }
+        }
+        try:
+            response = requests.post(url,headers=cls.headers,json=payload)
+
+            if response.status_code==200:
+                return "Question ajouter"
+            else:
+                return f"erreur lors de l'ajout {response.status_code}"
+        except Exception as e:
+            return f"erreur de connection {e}"
--- views/form.py
+++ views/form.py
@@ -1,14 +1,37 @@
 import customtkinter
 # on met. => pour dire que le fichier est dans le meme dossier 
 from  .btn_form import BtnForm
+from api.notion_api import NotionAPI
 
 class Form(customtkinter.CTkFrame):
     def __init__(self,master):
         super().__init__(master,fg_color="#F9F9F9")
-        
+        # Pour saisir du texte 
         self.question = customtkinter.CTkEntry(self,placeholder_text="Test a saisir");
         self.question.grid(row=2,column=1, padx=3,pady=3)
-
-        self.btn_frame = BtnForm(self)
+        # Pour le bouton
+        self.btn_frame = BtnForm(self,command=self.valider_formulaire)
         self.btn_frame.grid(row=2,column=2,pady=3,padx=3)
+
+        self.message_label = customtkinter.CTkLabel(self, text="")
+        self.message_label.grid(row=3, column=1, columnspan=2, pady=10)
+
+    def valider_formulaire(self):
+        question = self.question.get()
+
+        # APPELLE DE NOTRE CLASSE API 
+        succes, message = NotionAPI.verification(question)
+
+        # MISE A JOURS DE L'INTERFACE 
+        self.message_label.configure(text=message)
+        if succes:
+            self.message_label.configure(text_color="green")
+            NotionAPI.ajout_question(question)
+            
+            # Efface le texte de l'index 0 à la fin
+            self.question.delete(0, "end")
+        else:
+            self.message_label.configure(text_color="red")
+        
+
 

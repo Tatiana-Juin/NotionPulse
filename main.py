572e7da8a902d215cf0d85017df009c203631737
Tatiana-Juin <username@users.noreply.github.com>
2026-03-11 11:56:02 +0100

    J'ai racourcie la fenetre

--- main.py
+++ main.py
@@ -11,19 +11,19 @@ class Main(customtkinter.CTk):
         self.est_afficher_recherche = False;
 
         self.title("Application Notion")
-        self.geometry("1200x600")
+        self.geometry("920x500")
         self.configure(fg_color="#F9F9F9")
 
         # La 0 et la 2 vont "pousser" pour que la 1 reste bien au centre
-        self.grid_columnconfigure(0, weight=1)
-        self.grid_columnconfigure(1, weight=1)
+        # self.grid_columnconfigure(0, weight=1)
+        # self.grid_columnconfigure(1, weight=1)
         # self.grid_columnconfigure(2, weight=1)
 
-        self.titre_page = customtkinter.CTkLabel(self,text="Application pour les lives",font=("Helvetica",24,"bold"))
-        self.titre_page.grid(row=0,column=0,columnspan=2,pady=20)
+        # self.titre_page = customtkinter.CTkLabel(self,text="Application pour les lives",font=("Helvetica",24,"bold"))
+        # self.titre_page.grid(row=0,column=0,columnspan=2,pady=20)
 
         # Titre pour la section question   
-        self.titre_question = customtkinter.CTkLabel(self, text="❓ Question récurente",font=("Helvetica",15))
+        self.titre_question = customtkinter.CTkLabel(self, text="❓ Question récurente",font=("Helvetica",15,"bold"))
         self.titre_question.grid(row=1,column=0,pady=(10,0))
 
         # Titre pour les recherches 
@@ -38,9 +38,7 @@ class Main(customtkinter.CTk):
         self.afficher_question = Liste(self, Config.DB_QUESTIONS, "Nom")
         self.afficher_recherche = Liste(self,Config.DB_RECHERCHES,"Nom")
         self.liste_visible = False;
-        # self.afficher_recherche = Liste(self,Config.DB_RECHERCHES,"Nom")
-        # self.afficher_question.grid(row=4,column=0,padx=3,pady=3)
-
+       
         # Pour voir les 10 premiere question on devra cliquer sur un button 
         self.btn = Btn(self,text="voir les questions",command=lambda: self.toggle("question"))
         self.btn.grid(row=3,column=0,padx=3,pady=3)
@@ -49,18 +47,7 @@ class Main(customtkinter.CTk):
         self.btn_recherche.grid(row=3,column=1,padx=3,pady=3)
 
     def toggle(self,type_liste):
-        # self.est_afficher = not self.est_afficher
-        # self.est_afficher_recherche = not self.est_afficher
-
-        # if self.est_afficher == True:
-        #     self.btn.configure(text="masquer les questions")
-        #     self.afficher_question.grid(row=4,column=0,padx=3,pady=3)
-        #     self.afficher_question.actualiser()
-        # else:
-        #     print("Action : Je cache la liste")
-        #     self.afficher_question.grid_forget()
-            
-        #     self.btn.configure(text="voir les questions")
+       
         if type_liste =="question":
             self.est_afficher = not self.est_afficher
 
@@ -88,4 +75,3 @@ class Main(customtkinter.CTk):
 
 s = Main()
 s.mainloop()
-
